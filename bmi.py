774143957ff469670fcfd3eadee9b37dab275c27
pmtw <pmtw@wp.pl>
2015-11-26 18:07:24 +0100

    qwerty1

---
+++ bmi.py
@@ -0,0 +1,26 @@
+# print(80/1.85**2)
+
+#print('podaj wage:')
+waga=float(input('podaj swoja wage prosze: '))
+#print('podaj wzrost:')
+wzrost=float(input('podaj swoj wzrost prosze: '))
+
+def bmi(waga,wzrost):
+    print(waga/wzrost)
+    wynik=waga/wzrost**2
+    return wynik
+
+print ('bmi= ',bmi(waga,wzrost))    
+#print wynik
+
+#
+
+
+w=float(input('podaj swoja w prosze: '))
+
+#waga=85
+#wzrost=1.82
+
+
+
+
---
+++ bmi_jedna_linia.py
@@ -0,0 +1,1 @@
+print("bmi= ",(float(input('podaj swoja wage prosze: ')))/((float(input('podaj swoj wzrost prosze: ')))**2)
